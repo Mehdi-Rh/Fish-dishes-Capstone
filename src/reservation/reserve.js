70accf6fb035fa2685b3b56fcd81d22fff68ee03
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-03-23 16:41:33 +0100

    added popup count)

--- src/reservation/reserve.js
+++ src/reservation/reserve.js
@@ -6,21 +6,18 @@ const startDate = document.getElementById('startDate');
 const endDate = document.getElementById('endDate');
 const reserveBtn = document.getElementById('reserveBtn');
 const reserveContainer = document.querySelector('.reservations');
+const form = document.getElementById('form');
 
 closeBtn.addEventListener('click', () => {
-    popupWindow.remove();
+  popupWindow.remove();
 });
 
 const reservationsBox = document.createElement('ul');
 reservationsBox.className = 'reservations-box';
 reserveContainer.appendChild(reservationsBox);
 
-reserve = (add) => {
-    reservationsBox.innerHTML += `
-        <li class="item">${add.startDate} - ${add.endDate} by ${add.name}</li>
-    `;
-}
-
-const insertInTo = document.addEventListener('load', () => {
-    reservationsBox.push(username.value, startDate.value, endDate.value)
+let count = 0;
+reserveBtn.addEventListener('click', () => {
+  count += 1;
+  document.getElementById('counter').innerHTML = count;
 })
