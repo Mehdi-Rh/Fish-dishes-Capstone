d1b7de90d3986ad22bb783031b334fff8cb4c2ae
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-03-26 14:56:07 +0100

    Set tests

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
       },
+      // {
+      //   test: /\.m?js$/,
+      //   exclude: /(node_modules|bower_components)/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env'],
+      //       plugins: ['@babel/plugin-proposal-object-rest-spread']
+      //     }
+      //   }
+      // }
     ],
   },
 };
