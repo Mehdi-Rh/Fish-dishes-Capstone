b6de6773551bfcd75455f9c594635ef68e33d82d
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-03-26 23:33:04 +0100

    Delete useless commented code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import './style.css';
-// import getData from './modules/getData.js';
 import displayItems from './modules/displayMain.js';
 import { addLikes } from './modules/likes.js';
 import displayPopup from './modules/commentPopup.js';
-// import { getComment, addComment } from './modules/comments.js';
-// import { addReservation, getReservation } from './modules/reservations.js';
 
 const row = document.querySelector('.row');
 const urlItems = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
@@ -13,8 +10,6 @@ const urlItems = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
 const urlStart = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
 const apiId = 't9up9m5T6uECqwieuC71';
 const urlLike = `${urlStart + apiId}/likes`;
-// const urlComment = `${urlStart + apiId}/comments`;
-// const urlReservation = `${urlStart + apiId}/reservations`;
 
 /* Run functionalities */
 displayItems(row, urlItems, urlLike);
