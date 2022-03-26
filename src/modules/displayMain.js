897d3242063cf153a523c29423aaf5d6f814827d
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-03-26 21:02:10 +0100

    push change

--- src/modules/displayMain.js
+++ src/modules/displayMain.js
@@ -24,8 +24,7 @@ export default async function displayItems(mealContainer, url, urlLike) {
           <p class="like">${likeNumber || 0} Likes</p>
         </div>
       </div>
-      <button class="comments" type="button">Comments</button>
-      <button class="reservationBtn" type="button">Reservations</button> 
+      <button class="comments" type="button">More</button>
     </div>
   `;
   });
