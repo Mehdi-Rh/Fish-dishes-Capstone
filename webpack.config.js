895a72ee3b8e8daa358f786ee79ee558bd365cf0
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-04-18 16:48:30 +0100

    Set public path

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '',
   },
   module: {
     rules: [
